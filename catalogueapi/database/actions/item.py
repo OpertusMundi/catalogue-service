d61643899b0772b8fd83cd42d912869c42ad6882
nleond <nikleonidakis@gmail.com>
2020-05-13 13:47:35 +0300

    id instead of item id (Closes #3)

--- catalogueapi/database/actions/item.py
+++ catalogueapi/database/actions/item.py
@@ -9,30 +9,30 @@ log = logging.getLogger(__name__)
 session = db.session
 
 def create_item(data):
-    if not 'item_id' in data or not is_valid_uuid(data['item_id']):
-        data['item_id'] = str(uuid.uuid4())
+    if not 'id' in data or not is_valid_uuid(data['id']):
+        data['id'] = str(uuid.uuid4())
     item = Item(data)
     session.add(item)
     session.commit()
-    log.info('Created item %s', data['item_id'])
+    log.info('Created item %s', data['id'])
 
-def update_item(item_id, data):
+def update_item(id, data):
 
-    item = session.query(Item).get(item_id)
-    data['item_id'] = item_id
+    item = session.query(Item).get(id)
+    data['id'] = id
     item.title = data.get('title')
     item.description = data.get('description')
     item.creator = data.get('creator')
     session.add(item)
     session.commit()
-    log.info('Updated item %s', item_id)
+    log.info('Updated item %s', id)
 
-def delete_item(item_id):
+def delete_item(id):
 
-    item = session.query(Item).get(item_id)
+    item = session.query(Item).get(id)
     session.delete(item)
     session.commit()
-    log.info('Deleted item %s', item_id)
+    log.info('Deleted item %s', id)
 
 def is_valid_uuid(uuid_to_test, version=4):
     try:
--- catalogueapi/database/model/item.py
+++ catalogueapi/database/model/item.py
@@ -6,16 +6,16 @@ from catalogueapi.database import db
 class Item(db.Model):
     __tablename__ = "items"
 
-    item_id = db.Column('id', db.Text, primary_key=True)
+    id = db.Column('id', db.Text, primary_key=True)
+
     title = db.Column('title', db.Text, nullable=False, index=True)
-    description = db.Column('description', db.Text,
-                nullable=False, index=True)
-    creator = db.Column('creator', db.Text, index=True)
+    description = db.Column('description', db.Text, index=True)
+    creator = db.Column('creator', db.Text)
     item_json = db.Column('item_json', db.JSON)
 
-    def __init__(self,data):
-        self.item_id = data.get('item_id')
+    def __init__(self, data):
+        self.id = data.get('id')
         self.title = data.get('title')
         self.description = data.get('description')
         self.creator = data.get('creator')
-        self.item_json = json.dumps(data)
+        self.item_json = data
