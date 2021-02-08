9de13e09556327d3c773dae3aabe609c6e074eb3
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2021-02-08 14:10:52 +0200

    App should listen to all connections

--- catalogueapi/app.py
+++ catalogueapi/app.py
@@ -52,7 +52,7 @@ def initialize_app(flask_app):
 def main():
     initialize_app(app)
     log.info('>>>>> Starting development server at http://{}/api/ <<<<<'.format(app.config['SERVER_NAME']))
-    app.run(debug=app.config['FLASK_DEBUG'])
+    app.run(host="0.0.0.0",port=5000,debug=app.config['FLASK_DEBUG']);
 
 
 if __name__ == "__main__":
