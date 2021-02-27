24824d9fefa11cb8e285256a4ab10201824e773d
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2021-02-27 02:03:16 +0200

    Add some basic tests

--- tests/functional/api_tests.py
+++ tests/functional/api_tests.py
@@ -4,14 +4,18 @@ import os
 
 app = None
 
+resources_dir = None
+
 #
 # Setup/Teardown module
 #
 
 def setup_module():
     global app
-    from . import app as _app
+    global resources_dir
+    from . import app as _app, resources_dir as _resources_dir
     app = _app
+    resources_dir = _resources_dir
     print(" === Setting up tests for module %s with %s"  % (__name__, app))
     pass
 
@@ -32,3 +36,11 @@ def test_get_documentation_1():
         r = res.get_json();
         assert not (r.get('swagger') is None)
 
+def test_draft_create_1():
+    with app.test_client() as client:
+        req_data = None
+        with open(os.path.join(resources_dir, 'draft-1.json')) as f: 
+            req_data = json.load(f)
+        res = client.post('/api/draft/create', json=req_data)
+        assert res.status_code == 200
+    
